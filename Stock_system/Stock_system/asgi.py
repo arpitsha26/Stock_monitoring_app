5cfffc2099f6e8890118da741fc175ccf93c7645
arpitsha26 <arpitsharma1263@gmail.com>
2024-11-14 22:53:21 +0530

    websocket connection working

--- Stock_system/Stock_system/asgi.py
+++ Stock_system/Stock_system/asgi.py
@@ -9,19 +9,16 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 from channels.routing import ProtocolTypeRouter, URLRouter
 import os
 from django.urls import path
-
 from django.core.asgi import get_asgi_application
-from Home.consumers import * 
+from channels.auth import AuthMiddlewareStack
+import Home.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Stock_system.settings')
 
-application = get_asgi_application()
-
-ws_patterns=[
-    path('ws/test/', TestConsumer )
-    
-]
 
 application = ProtocolTypeRouter({
-    'websocket': URLRouter(ws_patterns)
+    "http":get_asgi_application(),
+    "websocket" : AuthMiddlewareStack(
+        URLRouter(Home.routing.websocket_urlpatterns)
+    )
 })
