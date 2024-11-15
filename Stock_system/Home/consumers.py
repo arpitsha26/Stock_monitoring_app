bfae8118898d58eaecdd5a449709636658e823b4
arpitsha26 <arpitsharma1263@gmail.com>
2024-11-15 20:15:17 +0530

    Fetch stock data using websockets

--- Stock_system/Home/consumers.py
+++ Stock_system/Home/consumers.py
@@ -1,7 +1,39 @@
-from channels.generic.websocket import WebsocketConsumer
 import json
+import asyncio
+from channels.generic.websocket import AsyncWebsocketConsumer
+import yfinance as yf
+from asgiref.sync import async_to_sync
 
-class TestConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
-        self.send(text_data=json.dumps({'status': 'connected'}))
+STOCKS = ['AMZN', 'TSLA', 'GOOGL', 'MSFT',]
+class StockConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        await self.send_stock_data()
+        
+    async def disconnect(self):
+        pass
+    
+    
+    async def receive(self, text_data):
+        pass
+        
+    
+    async def send_stock_data(self):
+        stock_data = await self.fetch_stock_data()
+        await self.send(json.dumps(stock_data))
+    
+    async def fetch_stock_data(self):
+        stock_details= {}
+        
+        for ticker in STOCKS:
+            stock = yf.Ticker(ticker)
+            stock_info = stock.history(period="1d")
+            current_price = stock.info.get('regularMarketPrice')
+            stock_details[ticker] = {
+                    "open": stock_info['Open'].iloc[-1],
+                    "close": stock_info['Close'].iloc[-1],
+                    "current_price": current_price, 
+                }
+        
+        return stock_details
+   
--- Stock_system/Home/routing.py
+++ Stock_system/Home/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from .import consumers
 
 websocket_urlpatterns=[
-re_path('ws/socket/', consumers.TestConsumer.as_asgi())    
+re_path('ws/stocks/', consumers.StockConsumer.as_asgi())    
 ]
