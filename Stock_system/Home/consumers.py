a82ea30bd45e7a215d6ebb260d037189307768cd
arpitsha26 <arpitsharma1263@gmail.com>
2024-11-16 21:14:47 +0530

    Updated:- Stock Models.py

--- Stock_system/Home/consumers.py
+++ Stock_system/Home/consumers.py
@@ -4,7 +4,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 import yfinance as yf
 from asgiref.sync import async_to_sync
 
-STOCKS = ['AMZN', 'TSLA', 'GOOGL', 'MSFT',]
+STOCKS = ["MSFT", "GOOGL", "AMZN", "TSLA", "RELIANCE.NS"]
 class StockConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
@@ -21,19 +21,27 @@ class StockConsumer(AsyncWebsocketConsumer):
     async def send_stock_data(self):
         stock_data = await self.fetch_stock_data()
         await self.send(json.dumps(stock_data))
+        await asyncio.sleep(10)
     
     async def fetch_stock_data(self):
         stock_details= {}
         
         for ticker in STOCKS:
-            stock = yf.Ticker(ticker)
-            stock_info = stock.history(period="1d")
-            current_price = stock.info.get('regularMarketPrice')
-            stock_details[ticker] = {
+            try:
+                 stock = yf.Ticker(ticker)
+                 stock_info = stock.history(period="1d")
+                 current_price = stock.info.get('regularMarketPrice')
+                 stock_details[ticker] = {
                     "open": stock_info['Open'].iloc[-1],
                     "close": stock_info['Close'].iloc[-1],
                     "current_price": current_price, 
                 }
+                 
+            except Exception as e:
+                stock_details[ticker]={
+                    "error": f"error in fetching: {str(e)}"
+                }
+            
         
         return stock_details
    
